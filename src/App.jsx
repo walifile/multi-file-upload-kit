405e40b4daaaaec66a515ae1c697ff6e7f92a83b
wali  ahmad <waliahmadfiles@gmail.com>
2024-03-21 16:13:43 +0500

    Update design: applied new styles and layout to file upload component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [error, setError] = useState("");
 
   const fileInputRef = useRef(null);
-  const acceptedFileExtensions = ["stl", "zip", "rar", "png"];
+  const acceptedFileExtensions = ["jpg", "png", "jpeg"];
 
   const acceptedFileTypesString = acceptedFileExtensions
     .map((ext) => `.${ext}`)
@@ -16,7 +16,6 @@ function App() {
     if (selectedFiles.length === 0) {
       setError("File is required");
     } else if (!error) {
-      onFilesChange([...selectedFiles]);
       setSelectedFiles([]);
       setError("");
     }
@@ -37,7 +36,7 @@ function App() {
     const newSelectedFiles = [...selectedFiles];
     let hasError = false;
     const fileTypeRegex = new RegExp(acceptedFileExtensions.join("|"), "i");
-
+    console.log(fileTypeRegex, "fileTypeRegex");
     filesArray.forEach((file) => {
       if (newSelectedFiles.some((f) => f.name === file.name)) {
         setError("File names must be unique");
@@ -66,110 +65,107 @@ function App() {
     updatedFiles.splice(index, 1);
     setSelectedFiles(updatedFiles);
   };
+
   return (
-    <>
-      <div className="border mt-2 border-neutral-300 rounded-xl shadow-md bg-white text-sm p-5 w-2/3 absolute z-20 text-primary-label">
-        <div>
-          <div className="mt-5 mb-3 flex justify-center">
-            {/* <CaseHeading heading="Please Upload Files" /> */}
-            Please Upload Files
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-full max-w-5xl p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-semibold text-center mb-4">
+          Upload Files
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div
+            className="min-h-[23rem] border-4 border-dashed border-blue-500 bg-blue-100 rounded-3xl p-4 flex flex-col justify-center items-center space-y-4"
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={(e) => handleDrop(e)}
+          >
+            <img
+              src="/assets/svg/upload.svg"
+              alt="Upload Icon"
+              className="w-24 h-24 mb-2"
+            />
+            <p className="text-lg font-semibold">Drag and Drop the files</p>
+            <p className="text-lg font-bold">or</p>
+            <button
+              type="button"
+              onClick={handleCustomButtonClick}
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Upload Files
+            </button>
+            <input
+              type="file"
+              id="files"
+              name="files"
+              multiple
+              accept={acceptedFileTypesString}
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleFileChange}
+              onClick={(event) => {
+                // Reset the input value to allow selecting the same file again
+                event.target.value = null;
+              }}
+            />
           </div>
-          <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div
-                className="min-h-[23rem]
-                 border-3 border-dashed border-[#0284C7] bg-[#F0F9FF] rounded-3xl p-4 flex flex-col justify-center items-center space-y-4"
-                onDragOver={(e) => e.preventDefault()}
-                onDrop={(e) => handleDrop(e)}
-              >
-                <img
-                  src="/assets/images/49.svg"
-                  alt="image"
-                  width={90}
-                  height={90}
-                />
 
-                <p className="text-lg font-semibold">Drag and Drop the files</p>
-                <p className="text-lg font-bold">or</p>
-
-                <button type="button" onClick={handleCustomButtonClick}>
-                  Upload Files
-                </button>
-
-                <input
-                  type="file"
-                  id="files"
-                  name="files"
-                  multiple
-                  accept={acceptedFileTypesString}
-                  ref={fileInputRef}
-                  style={{ display: "none" }}
-                  onClick={(event) => {
-                    // Reset the input value to allow selecting the same file again
-                    event.target.value = null;
-                  }}
-                  onChange={handleFileChange}
-                  className="form-control"
-                />
+          <div className="border-2 border-gray-300 rounded-3xl py-4 max-h-[23rem] overflow-auto">
+            {selectedFiles.length > 0 ? (
+              <ul className="px-4">
+                {selectedFiles.map((file, index) => (
+                  <li
+                    key={file.name}
+                    className="flex justify-between items-center border-b py-2"
+                  >
+                    <div className="flex items-center">
+                      <img
+                        src="/assets/svg/image.svg"
+                        alt="File Icon"
+                        className="w-8 h-8 mr-2"
+                      />
+                      <span className="text-base">{file.name}</span>
+                    </div>
+                    <button
+                      type="button"
+                      onClick={() => handleFileDelete(index)}
+                      className="text-red-500 hover:text-red-700 focus:outline-none"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-width="2"
+                          d="M6 4l8 8M14 4l-8 8"
+                        />
+                      </svg>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="h-full flex justify-center items-center">
+                <p className="text-lg font-semibold text-gray-500 text-center">
+                  No Files Uploaded Yet
+                </p>
               </div>
-
-              <div
-                className={`rounded-3xl border-2 border-[#A3A3A3] py-4 max-h-[23rem]`}
-              >
-                <div className="h-full overflow-auto  py-2 px-5">
-                  {selectedFiles.length > 0 ? (
-                    <ul>
-                      {selectedFiles?.map((file, index) => (
-                        <div
-                          className="flex justify-between items-center"
-                          key={file.name}
-                        >
-                          <div className="flex items-center pb-2">
-                            <img
-                              src="/assets/images/50.svg"
-                              width={40}
-                              height={40}
-                              alt="icon"
-                            />
-                            <span
-                              className="text-lg underline ps-4"
-                              title={file.name}
-                            >
-                              {file.name}
-                            </span>
-                          </div>
-                          <div>
-                            CloseIcon
-                            {/* <CloseIcon
-                              color="#444444"
-                              onClick={() => handleFileDelete(index)}
-                            /> */}
-                          </div>
-                        </div>
-                      ))}
-                    </ul>
-                  ) : (
-                    <p className="text-lg font-semibold  text-[#A3A3A3] h-full flex items-center justify-center">
-                      No Files Uploaded Yet
-                    </p>
-                  )}
-                </div>
-                {error && (
-                  <div style={{ color: "red" }} className={`pt-5 pl-2 `}>
-                    {error}
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="flex justify-center mt-8">
-              <button type="submit" onClick={handleSubmit}>
-                Save
-              </button>
-            </div>
+            )}
           </div>
         </div>
+        {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
+        <div className="flex justify-center mt-8">
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Save
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
